0a5fdffbdbc7457c8e2025c92180885e154a23f0
yasin2211 <123598308+yasin2211@users.noreply.github.com>
2024-01-04 23:26:53 +0330

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+function change0() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back.webp')";
+}
+
+function change1() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back2.webp')";
+}
+
+function change2() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back3.webp')";
+}
+
+function change3() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back4.webp')";
+}
+
+function change4() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back5.webp')";
+}
+
+function change5() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back6.webp')";
+}
+
+function change6() {
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back7.webp')";
+}
