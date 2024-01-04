b1a726143c7b6212326d2b765f10b7932fdcd44b
yasin2211 <123598308+yasin2211@users.noreply.github.com>
2024-01-04 23:38:10 +0330

    Update java.js

--- java.js
+++ java.js
@@ -1,27 +1,27 @@
 function change0() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back.webp')";
 }
 
 function change1() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back2.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back2.webp')";
 }
 
 function change2() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back3.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back3.webp')";
 }
 
 function change3() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back4.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back4.webp')";
 }
 
 function change4() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back5.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back5.webp')";
 }
 
 function change5() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back6.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back6.webp')";
 }
 
 function change6() {
-    document.getElementById("back-iamge-sec").style.backgroundImage = "url('./pic/back7.webp')";
+    document.getElementById("back-iamge-sec").style.backgroundImage = "url('back7.webp')";
 }
